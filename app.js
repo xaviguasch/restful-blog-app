c619b54351756f500d1f7af1f41fa5ff54497082
xaviguasch <xaviguasch@gmail.com>
2019-01-07 12:40:03 +0100

    adding editing & updating to blog posts

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 
 // APP CONFIG
 mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useNewUrlParser: true })
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 
 // MOONGOSE/MODEL CONFIG
@@ -69,10 +71,30 @@ app.get('/blogs/:id', function(req, res){
         } else {
             res.render('show', {blog: foundBlog})
         }
-
     })
 })
 
+// EDIT ROUTE
+app.get('/blogs/:id/edit', function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect('/blogs')
+        } else {
+            res.render('edit', {blog: foundBlog})
+        }
+    })
+})
+
+// UPDATE ROUTE
+app.put('/blogs/:id', function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect('/blogs')
+        } else {
+            res.redirect(`/blogs/${req.params.id}`)
+        }
+    })
+})
 
 
 
