3cff47ac8bd6cf5f43f39ca0a1409d57cbcafc0c
xaviguasch <xaviguasch@gmail.com>
2019-01-07 13:28:42 +0100

    adding express-sanitizer

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
+const expressSanitizer = require('express-sanitizer')
 
 // APP CONFIG
 mongoose.connect('mongodb://localhost:27017/restful_blog_app', { useNewUrlParser: true })
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(expressSanitizer()) // it needs to go AFTER bodyParser
 app.use(methodOverride('_method'))
 
 
@@ -53,6 +55,10 @@ app.get('/blogs/new', function(req, res){
 // CREATE ROUTE
 app.post('/blogs', function(req, res){
     // create blog
+    console.log(req.body);
+    
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+    console.log(req.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render('new')
